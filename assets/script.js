1113d4503696124ea5d0265df52bbf01082ac689
kadereub <reuben.kade@gmail.com>
2020-03-23 22:10:31 +0000

    add google analytics

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-64322173-2', 'auto');
+
+/* <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-64322173-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-64322173-2');
+</script> */
