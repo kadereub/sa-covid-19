4866063ba5c94c38e2c58860ddff177c9fb39efd
kadereub <reuben.kade@gmail.com>
2020-03-23 21:13:52 +0000

    minor refactor

--- app.py
+++ app.py
@@ -339,14 +339,14 @@ app.layout = html.Div(
             For the most recent information on COVID-19 please see the Government website above. Say safe, and wash thy hands!
         ''', html.Br(), html.Br(),
                          html.P("Contact Email: sacovidtracker@gmail.com"),
-                         html.Br(), html.Br(),
+                         html.Br(),
                          html.A(
                              "GitHub Code Repo - Log Issues Here!",
                              href="https://github.com/kadereub/sa-covid-19",
                  )],
                style={"text-align":"center", "font-weight": "bold"}
         ),
-            html.Br(), html.Br()
+            html.Br()
         ]
     )
 
